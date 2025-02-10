c2432899627b6b17d71aaae1538437fd9725cf60
Alikhan <fixator2004@gmail.com>
2025-02-09 23:20:32 -0500

    Task 2 - Sales Tax Computation.

--- cc_6.js
+++ cc_6.js
@@ -8,3 +8,14 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){   //declared a fun
 console.log(`Total Profit: $${calculateProfit(20, 30, 100)}`); //logged total profit for 2 sets of numbers
 console.log(`Total Profit: $${calculateProfit(50, 70, 200)}`);
 
+//Task 2: Function Expression
+
+function calculateSalesTax(amount, taxRate){    //declared a function that multiplies amount by sales tax and used to fixed to round to 2 decimals
+
+    return (amount * taxRate).toFixed(2);
+};
+
+console.log(`Sales Tax: $${calculateSalesTax(100, 0.07)}`); //logged the output for the given sets of numbers
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.01)}`);
+
+
